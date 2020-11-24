4e0392b3c672d2cea23bed4b5d6f3bc99abde942
Stratadox <info@stratadox.com>
2020-11-25 00:45:13 +0100

    Add (minimal) static source

---
+++ __tests__/fetching_conversation_starters.test.js
@@ -0,0 +1,19 @@
+const Bot = require('../src/Bot.js');
+const StaticSource = require('../src/Source/StaticSource.js');
+
+test('fetches the source with the matching tag', () => {
+    let message = '';
+    const bot = new Bot();
+
+    bot.addSource(new StaticSource('expected', ['tag']));
+    bot.addSource(new StaticSource('unexpected', ['no']));
+
+    bot.configure('test-channel', 10, function (msg) {
+        message = msg;
+    }, ['tag']);
+
+    bot.notify('test-channel', new Date(0));
+    bot.reviveChannels(new Date(15 * 1000));
+
+    expect(message).toBe('expected');
+});
--- src/Bot.js
+++ src/Bot.js
@@ -1,7 +1,10 @@
+/**
+ * @property {StaticSource[]} sources
+ */
 class Bot {
-
     constructor() {
         this.channels = new Map();
+        this.sources = [];
     }
 
     /**
@@ -14,18 +17,28 @@ class Bot {
             this.channels.get(channel).updated = date;
         }
     }
+
     /**
      * Configure a new channel
      * @param  {String}   channel  Channel ID
      * @param  {Number}   delay    Time between bumps in seconds
      * @param  {Function} callback Callback to execute upon revival
+     * @param  {Array}    tags     Tags to considered relevant
      * @throws When refused
      */
-    configure(channel, delay, callback) {
+    configure(channel, delay, callback, tags = []) {
         if(delay < 10){
             throw `That's too often! Choose a time over 10 seconds.`;
         }
-        this.channels.set(channel, {callback, delay, updated: new Date()});
+        this.channels.set(channel, {callback, delay, updated: new Date(), tags: tags});
+    }
+
+    /**
+     * Configure a new source
+     * @param {StaticSource} source
+     */
+    addSource(source) {
+        this.sources.push(source);
     }
 
     /**
@@ -44,9 +57,26 @@ class Bot {
     reviveChannels(date) {
         this.channels.forEach(channel => {
             if (date.getTime() - channel.updated.getTime() > channel.delay * 1000) {
-                channel.callback();
+                let source = this.chooseSourceFor(channel);
+                channel.callback(source ? source.getMessage() : '');
             }
         });
+    }
+
+    /**
+     * Chooses a relevant source for the channel
+     * @param {Object} channel
+     * @return {StaticSource}
+     * @todo pick one at random
+     * @todo add weighted random, where weight is # of tags in common
+     */
+    chooseSourceFor(channel) {
+        for (let i in this.sources) {
+            let source = this.sources[i];
+            if (source.isRelevantToAnyOfThese(channel.tags)) {
+                return source;
+            }
+        }
     }
 }
 
