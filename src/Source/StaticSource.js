586835f8423768fec2563031420db685095eee41
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-11-24 17:23:59 -0800

    feat: generic Source

--- src/Source/StaticSource.js
+++ src/Source/StaticSource.js
@@ -1,25 +1,13 @@
-class StaticSource {
+const Source = require('./Source.js');
+
+class StaticSource extends Source {
     /**
      * @param {string} message The message to respond with
      * @param {string[]} tags  The tags to respond to
      */
     constructor(message, tags) {
+        super(tags);
         this.message = message;
-        this.tags = tags;
-    }
-
-    /**
-     * Check if the source has any of the given tags
-     * @param {string[]} tags The tags we're looking for
-     * @return {boolean}      Whether one of the tags corresponds to our own
-     */
-    isRelevantToAnyOfThese(tags) {
-        for (let tag in this.tags) {
-            if (tags.includes(this.tags[tag])) {
-                return true;
-            }
-        }
-        return false;
     }
 
     /**
