375fb46e73579369aca298f49441af2a6ca9856c
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-11-24 19:48:28 -0800

    fix: make tests penetrate Bot less

--- __tests__/fetching_conversation_starters.test.js
+++ __tests__/fetching_conversation_starters.test.js
@@ -1,7 +1,7 @@
 const Bot = require('../src/Bot.js');
 const StaticSource = require('../src/Source/StaticSource.js');
 
-test('fetches the source with the matching tag', () => {
+test('fetches the source with the matching tag', done => {
     let message = '';
     const bot = new Bot();
 
@@ -13,9 +13,17 @@ test('fetches the source with the matching tag', () => {
     bot.configureChannel('test-channel', {
         delay: 10,
         callback: msg => {
-            message = msg;
+            try{
+                expect(msg).toBe('expected')
+                done();
+            } catch (e){
+                done(e)
+            }
         },
         tags: ['tag']
     });
-    expect(bot.chooseSourceFor(bot.channels.get('test-channel'))).toBe(correctSource);
+
+    bot.notify('test-channel', new Date(0));
+    bot.reviveChannels(new Date(15 * 1000));
+
 });
