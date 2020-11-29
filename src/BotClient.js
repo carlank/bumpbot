a9bf7d7c9d6d5515b029ac88c2ba42d2e598d58b
Nick Sullivan-Friedman <rpgcubed@gmail.com>
2020-11-28 23:30:56 -0800

    feat: add WebSource and example

--- src/BotClient.js
+++ src/BotClient.js
@@ -1,7 +1,7 @@
 import {Client} from 'discord.js';
 import Bot from './Bot';
 import XkcdSource from './Source/XkcdSource.js';
-
+import WebSource from './Source/WebSource.js'
 /**
  * An extension of the discord.js Client to include a Bot, to simplify modular commands.
  * @class
@@ -16,6 +16,11 @@ export default class BotClient extends Client{
 
         this.bot = new Bot();
         this.bot.addSource(new XkcdSource(['xkcd']));
+        this.bot.addSource(new WebSource({
+          url: 'https://meowfacts.herokuapp.com/',
+          handler: data => data.data[0],
+          tags: ['cat']
+        }));
 
         this.login();
     }
