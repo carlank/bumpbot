049049d612342196e376ce9bb12cfadcb645bc6f
klueck21 <70820765+klueck21@users.noreply.github.com>
2020-12-03 12:17:59 -0600

    changed the message!!

--- src/commands/bump.js
+++ src/commands/bump.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'bump',
     description: 'Bumps the channel once',
     execute(client, message) {
-        message.channel.send('Wake up!');
+        message.channel.send('Remember anyone of you guys can become a champion!! Feel free to apply!);
     }
 };
